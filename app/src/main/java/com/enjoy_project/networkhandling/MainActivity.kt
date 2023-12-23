e7d8a57fc5c2b45280e2258c9464590343455b5a
choeuihyeon <amammmaa1@naver.com>
2023-12-23 21:24:47 +0900

    테스트를 위한 액티비티 구현

--- app/src/main/java/com/enjoy_project/networkhandling/MainActivity.kt
+++ app/src/main/java/com/enjoy_project/networkhandling/MainActivity.kt
@@ -2,10 +2,33 @@ package com.enjoy_project.networkhandling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val retrofit = RetrofitImpl()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testButton = findViewById<Button>(R.id.testButton)
+        val testText = findViewById<TextView>(R.id.testText)
+
+        testButton.setOnClickListener {
+
+            retrofit.getUserList {
+
+                Log.d("test", it.toString())
+
+                testText.text = it.toString()
+
+            }
+
+
+        }
+
     }
 }
